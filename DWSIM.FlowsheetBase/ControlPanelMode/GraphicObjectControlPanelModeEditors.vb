6e6bd789a175f88e6b3d83a7341112d46de51a09
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2025-02-17 17:34:22 -0300

    added CPUI form for input objects (#715)

--- DWSIM.FlowsheetBase/ControlPanelMode/GraphicObjectControlPanelModeEditors.vb
+++ DWSIM.FlowsheetBase/ControlPanelMode/GraphicObjectControlPanelModeEditors.vb
@@ -1,36 +1,46 @@
 ﻿Imports DWSIM.ExtensionMethods
 Imports DWSIM.Interfaces
 Imports DWSIM.SharedClasses
-Imports System.Linq
-Imports System.Windows.Forms
+Imports Eto.Forms
 
 Public Class GraphicObjectControlPanelModeEditors
 
+    Private Shared Function CreateForm() As Dialog
+
+        Dim tb As New TextBox With {.Width = 100}
+        Dim f As New Dialog
+        f.Content = tb
+        f.AutoSize = True
+        Return f
+
+    End Function
+
     Public Shared Sub SetInputDelegate(gobj As IGraphicObject, myObj As ISimulationObject)
 
-        'gobj.ControlPanelModeEditorDisplayDelegate = Sub()
-        '                                                 Dim f As New FormTextBoxInput
-        '                                                 Dim SelectedObject = myObj?.GetFlowsheet.SimulationObjects.Values.Where(Function(x2) x2.Name = myObj.SelectedObjectID).FirstOrDefault
-        '                                                 If Not SelectedObject Is Nothing Then
-        '                                                     Dim currentvalue = SystemsOfUnits.Converter.ConvertFromSI(myObj.SelectedPropertyUnits, SelectedObject.GetPropertyValue(myObj.SelectedProperty))
-        '                                                     f.TextBox1.Text = currentvalue.ToString(myObj?.GetFlowsheet.FlowsheetOptions.NumberFormat)
-        '                                                     f.Text = SelectedObject.GraphicObject.Tag + "/" + myObj?.GetFlowsheet.GetTranslatedString(myObj.SelectedProperty)
-        '                                                     AddHandler f.TextBox1.KeyDown,
-        '                                                     Sub(s, e)
-        '                                                         If e.KeyCode = Keys.Enter Then
-        '                                                             Try
-        '                                                                 SelectedObject.SetPropertyValue(myObj.SelectedProperty, f.TextBox1.Text.ToDoubleFromCurrent().ConvertToSI(myObj.SelectedPropertyUnits))
-        '                                                                 f.Close()
-        '                                                             Catch ex As Exception
-        '                                                                 MessageBox.Show("Error", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error)
-        '                                                             End Try
-        '                                                         End If
-        '                                                     End Sub
-        '                                                     f.StartPosition = FormStartPosition.Manual
-        '                                                     f.Location = Cursor.Position
-        '                                                     f.ShowDialog()
-        '                                                 End If
-        '                                             End Sub
+        gobj.ControlPanelModeEditorDisplayDelegate =
+            Sub()
+                Dim f = CreateForm()
+                Dim tb = DirectCast(f.Content, TextBox)
+                Dim SelectedObject = myObj?.GetFlowsheet.SimulationObjects.Values.Where(Function(x2) x2.Name = myObj.SelectedObjectID).FirstOrDefault
+                If Not SelectedObject Is Nothing Then
+                    Dim currentvalue = SystemsOfUnits.Converter.ConvertFromSI(myObj.SelectedPropertyUnits, SelectedObject.GetPropertyValue(myObj.SelectedProperty))
+                    tb.Text = currentvalue.ToString(myObj?.GetFlowsheet.FlowsheetOptions.NumberFormat)
+                    f.Title = SelectedObject.GraphicObject.Tag + "/" + myObj?.GetFlowsheet.GetTranslatedString(myObj.SelectedProperty)
+                    AddHandler tb.KeyDown,
+                    Sub(s, e)
+                        If e.Key = Keys.Enter Then
+                            Try
+                                SelectedObject.SetPropertyValue(myObj.SelectedProperty, tb.Text.ToDoubleFromCurrent().ConvertToSI(myObj.SelectedPropertyUnits))
+                                f.Close()
+                            Catch ex As Exception
+                                MessageBox.Show("Error", ex.Message, MessageBoxButtons.OK, MessageBoxType.Error)
+                            End Try
+                        End If
+                    End Sub
+                    f.Location = Mouse.Position
+                    f.ShowModal()
+                End If
+            End Sub
 
 
     End Sub
