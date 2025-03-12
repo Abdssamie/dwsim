8c96977df32ff37a60fb7ac5930fe4f1829734be
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2025-03-12 16:18:25 -0300

    automation update

--- DWSIM.Automation/Automation.cs
+++ DWSIM.Automation/Automation.cs
@@ -658,6 +658,20 @@ namespace DWSIM.Automation
             return f;
         }
 
+        [DispId(11)]
+        public void CalculateFlowsheet5(IFlowsheet flowsheet, int timeout_seconds)
+        {
+            Settings.SolverTimeoutSeconds = timeout_seconds;
+            ((Flowsheet2)flowsheet).SolveFlowsheet3();
+        }
+
+        [DispId(12)]
+        public List<Exception> CalculateFlowsheet6(IFlowsheet flowsheet)
+        {
+            return ((Flowsheet2)flowsheet).SolveFlowsheet3();
+        }
+
+
         public object GetMainWindow()
         {
             throw new NotImplementedException();
