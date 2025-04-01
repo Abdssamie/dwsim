e1706276e8594a69a8e5e0fadb75d39cf27e2ccd
Mersad Katana <mersadk@gmail.com>
2025-04-01 09:57:22 +0200

    Update file saving events

--- DWSIM.Simulate365/Services/FileUploaderService.cs
+++ DWSIM.Simulate365/Services/FileUploaderService.cs
@@ -16,65 +16,9 @@ using System.Threading.Tasks;
 
 namespace DWSIM.Simulate365.Services
 {
-
-
-
     public static class FileUploaderService
     {
-        public static ConcurrentDictionary<string, Func<Task<UploadNotAllowedEventArgs>>> Validators = new ConcurrentDictionary<string, Func<Task<UploadNotAllowedEventArgs>>>();
-
-        public static event EventHandler<UploadNotAllowedEventArgs> OnFileNotAllowedToBeSaved;
-
-
-        private static async Task<bool> CanUploadFileAsync()
-        {
-            if (!Validators.Any())
-                return true;
-
-            try
-            {
-                // Execute all validators concurrently
-                var validatorTasks = Validators.Values;
-
-                var canUploadResults = new List<UploadNotAllowedEventArgs>();
-                // Wait for all tasks to complete
-
-                foreach (var validatorTask in validatorTasks)
-                {
-                    var result = Task.Run(validatorTask).Result;
-
-                    if (result != null)
-                        canUploadResults.Add(result);
-                }
-
-
-                // Check the results
-                foreach (var result in canUploadResults)
-                {
-                    if (result != null)
-                    {
-                        // Raise event for each validation result
-                        OnFileNotAllowedToBeSaved?.Invoke(null, result);
-                    }
-                }
-
-                // If any validator result is not null, return false
-                if (canUploadResults.Any(x => x != null))
-                {
-                    var reason = canUploadResults.First().Reason;
-                    throw new Exception(reason);
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-                // Handle exception if necessary
-                throw;
-            }
-
-            return true;
-        }
+        public static event EventHandler<BeforeUploadEventArgs> BeforeUpload;
 
         public static S365File UploadFile(string fileUniqueIdentifier, string parentUniqueIdentifier, string filePath, string filename, string simulatePath, UploadConflictAction? conflictAction)
         {
@@ -82,11 +26,46 @@ namespace DWSIM.Simulate365.Services
                 return UploadFile(fileUniqueIdentifier, parentUniqueIdentifier, fileStream, filename, simulatePath, conflictAction);
         }
 
-        public static S365File UploadFileByFilePath(string simulatePath, Stream fileStream, UploadConflictAction? conflictAction)
+        public static S365File UploadFile(string fileUniqueIdentifier, string parentUniqueIdentifier, Stream fileStream, string filename, string simulatePath, UploadConflictAction? conflictAction)
         {
-            var canUpload = CanUploadFileAsync().Result;
             try
             {
+                // Invoke event handlers
+                var eventArgs = new BeforeUploadEventArgs();
+                BeforeUpload?.Invoke(null, eventArgs);
+                if (eventArgs.Cancel)
+                    throw new Exception("Upload operation was canceled.");
+
+                fileStream.Seek(0, SeekOrigin.Begin);
+
+                var token = UserService.GetInstance().GetUserToken();
+                var client = GetDashboardClient(token);
+
+                var file = Task.Run(async () => await UploadDocumentAsync(parentUniqueIdentifier, filename, fileStream, conflictAction)).Result;
+
+                return new S365File(filename)
+                {
+                    FileUniqueIdentifier = file.FileUniqueIdentifier.ToString(),
+                    ParentUniqueIdentifier = parentUniqueIdentifier,
+                    Filename = file.Filename,
+                    FullPath = file.SimulatePath
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while saving file to Simulate 365 Dashboard.", ex);
+            }
+        }
+
+        public static S365File UploadFileByFilePath(string simulatePath, Stream fileStream, UploadConflictAction? conflictAction)
+        {
+            try
+            {
+                // Invoke event handlers
+                var eventArgs = new BeforeUploadEventArgs();
+                BeforeUpload?.Invoke(null, eventArgs);
+                if (eventArgs.Cancel)
+                    throw new Exception("Upload operation was canceled.");
 
                 if (simulatePath.StartsWith("//Simulate 365 Dashboard/"))
                     simulatePath = simulatePath.Substring(24);
@@ -112,50 +91,16 @@ namespace DWSIM.Simulate365.Services
                     ParentUniqueIdentifier = parentUniqueIdentifier,
                     Filename = fileResp.Filename,
                     FullPath = fileResp.SimulatePath
-
                 };
-
             }
             catch (Exception ex)
             {
-
                 throw new Exception("An error occurred while saving file to Simulate 365 Dashboard.", ex);
             }
-        }
-
-        public static S365File UploadFile(string fileUniqueIdentifier, string parentUniqueIdentifier, Stream fileStream, string filename, string simulatePath, UploadConflictAction? conflictAction)
-        {
-            var canUpload = CanUploadFileAsync().Result;
-            try
-            {
-                fileStream.Seek(0, SeekOrigin.Begin);
-
-                var token = UserService.GetInstance().GetUserToken();
-                var client = GetDashboardClient(token);
-
-                var file = Task.Run(async () => await UploadDocumentAsync(parentUniqueIdentifier, filename, fileStream, conflictAction)).Result;
-
-                return new S365File(filename)
-                {
-                    FileUniqueIdentifier = file.FileUniqueIdentifier.ToString(),
-                    ParentUniqueIdentifier = parentUniqueIdentifier,
-                    Filename = file.Filename,
-                    FullPath = file.SimulatePath
-
-                };
-
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception("An error occurred while saving file to Simulate 365 Dashboard.", ex);
-            }
-        }
+        }        
 
         private static async Task<UploadFileResponseModel> UploadDocumentAsync(string parentUniqueIdentifier, string filename, Stream fileStream, UploadConflictAction? conflictAction)
         {
-
-
             try
             {
                 var token = UserService.GetInstance().GetUserToken();
@@ -165,13 +110,9 @@ namespace DWSIM.Simulate365.Services
                 {
                     // 0= Overwrite file if exists, 1= Keep both
                     if (conflictAction.HasValue)
-                    {
                         content.Add(new StringContent(conflictAction.ToString()), "ConflictAction");
-                    }
                     if (!string.IsNullOrWhiteSpace(parentUniqueIdentifier))
-                    {
                         content.Add(new StringContent(parentUniqueIdentifier), "ParentDirectoryUniqueId");
-                    }
 
                     content.Add(new StreamContent(fileStream), "files", filename);
 
@@ -187,22 +128,19 @@ namespace DWSIM.Simulate365.Services
                         throw new Exception($"An error occurred while uploading file. Status code: {response.StatusCode}. Error:{errorMessage}");
                     }
                     if (responseModel == null || responseModel.Count == 0)
-                    {
                         throw new Exception("An error occurred while uploading file. Response is empty.");
-                    }
 
                     return responseModel.First();
                 }
-
             }
             catch (Exception ex)
             {
                 throw new Exception("An error occurred while trying to upload document.", ex);
             }
         }
+
         private static FilesWithBreadcrumbsResponseModel GetFileByPath(string simulatePath)
         {
-
             var token = UserService.GetInstance().GetUserToken();
             var client = GetDashboardClient(token);
             var result = Task.Run(async () => await client.GetAsync($"/api/files/by-path?filePath={simulatePath}&includeBreadcrumbs=true")).Result;
@@ -218,13 +156,11 @@ namespace DWSIM.Simulate365.Services
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
 
             return client;
-
         }
-
     }
-    public class UploadNotAllowedEventArgs : EventArgs
+
+    public class BeforeUploadEventArgs : EventArgs
     {
-        public string EventKey { get; set; }
-        public string Reason { get; set; }
+        public bool Cancel { get; set; }
     }
 }
