b1a906a11b9f42c121b8fa3164064d9f5a142b60
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2025-04-23 09:53:42 -0300

    upped version number

--- DWSIM.ProFeatures/My Project/AssemblyInfo.cs
+++ DWSIM.ProFeatures/My Project/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("9.0.1.0")]
-[assembly: AssemblyFileVersion("9.0.1.0")]
+[assembly: AssemblyVersion("9.0.2.0")]
+[assembly: AssemblyFileVersion("9.0.2.0")]
